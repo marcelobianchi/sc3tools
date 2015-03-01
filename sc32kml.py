00e2e3e101788c9aeb94627cd2c5e770c2c82aee
Marcelo Bianchi <m.bianchi@iag.usp.br>
2015-02-28 22:22:33 -0300

    Added license preemble. Added comment on max depth

--- sc32kml.py
+++ sc32kml.py
@@ -1,13 +1,27 @@
-#!/usr/bin/python
-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
 ################################################################################
 #                                                                              #
-# SeisComp3 XML to KML converter                                               #
-# written by Mb @ USP                                                          #
-# this is free software                                                        #
+#   SeisComp3 XML to KML converter                                             #
+#   Copyright (C) 2015  Marcelo Belentani de Bianchi                           #
+#                                                                              #
+#   This program is free software; you can redistribute it and/or modify       #
+#   it under the terms of the GNU General Public License as published by       #
+#   the Free Software Foundation; either version 2 of the License, or          #
+#   (at your option) any later version.                                        #
+#                                                                              #
+#   This program is distributed in the hope that it will be useful,            #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
+#   GNU General Public License for more details.                               #
+#                                                                              #
+#   You should have received a copy of the GNU General Public License along    #
+#   with this program; if not, write to the Free Software Foundation, Inc.,    #
+#   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.                #
 #                                                                              #
 #### 2015-02-26 ################################################################
-
+#
 import sys, hashlib, math
 from seiscomp3 import IO, DataModel
 from optparse import OptionParser
@@ -287,7 +301,8 @@ if __name__ == "__main__":
 			style = styler.basicstyle()
 
 		if options.skydepth:
-			data['dep'] = -0.5 * (950.0 - data['dep'])
+			# Maximum earths eq depth is ~1000km
+			data['dep'] = -0.5 * (1000.0 - data['dep'])
 
 		data['style'] = style
 		files.append(data)
