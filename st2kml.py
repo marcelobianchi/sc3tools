189a7b426550245c1aed6cb5c0a15f0ce70e8f86
Marcelo Bianchi <m.bianchi@iag.usp.br>
2024-08-14 17:38:33 -0300

    migrated python3 + seiscomp6

--- st2kml.py
+++ st2kml.py
@@ -24,9 +24,9 @@
 #
 
 import sys, hashlib, math
-from seiscomp3 import IO, DataModel
+from seiscomp import io as IO, datamodel as DataModel
 from optparse import OptionParser
-import seiscomp3
+import seiscomp
 import datetime, re
 
 '''
@@ -41,19 +41,19 @@ class StyleFactory(object):
             color = self.styles[ID]['color']
             scale = self.styles[ID]['size']
 
-            print >>openfile,'<Style id="%s">' % ID
-            print >>openfile,'<LabelStyle>'
-            print >>openfile,'<scale>0</scale>'
-            print >>openfile,'</LabelStyle>'
-            print >>openfile,'<IconStyle>'
+            print ('<Style id="%s">' % ID, file = openfile)
+            print ('<LabelStyle>', file = openfile)
+            print ('<scale>0</scale>', file = openfile)
+            print ('</LabelStyle>', file = openfile)
+            print ('<IconStyle>', file = openfile)
             if color:
-                print >>openfile,' <color>%s</color>' % color
-            print >>openfile,' <scale>%f</scale>' % scale
-            print >>openfile,' <Icon>'
-            print >>openfile,'  <href>http://maps.google.com/mapfiles/kml/shapes/triangle.png</href>'
-            print >>openfile,' </Icon>'
-            print >>openfile,'</IconStyle>'
-            print >>openfile,'</Style>'
+                print (' <color>%s</color>' % color, file = openfile)
+            print (' <scale>%f</scale>' % scale, file = openfile)
+            print (' <Icon>', file = openfile)
+            print ('  <href>http://maps.google.com/mapfiles/kml/shapes/triangle.png</href>', file = openfile)
+            print (' </Icon>', file = openfile)
+            print ('</IconStyle>', file = openfile)
+            print ('</Style>', file = openfile)
         return
 
     def basicstyle(self):
@@ -64,7 +64,7 @@ class StyleFactory(object):
         return "basic"
 
     def getstyle(self, size, color):
-        sh1 = "S_%s" %  hashlib.sha1("%.2f-%s" %  (size, color)).hexdigest()
+        sh1 = "S_%s" %  hashlib.sha1(f"{size:.2f}-%{color}".encode()).hexdigest()
         if sh1 in self.styles:
             return sh1
         self.styles[sh1] = { }
@@ -74,19 +74,19 @@ class StyleFactory(object):
         return sh1
 
 def newFolder(openfile, name):
-    print >>openfile,' <Folder>'
-    print >>openfile,'  <name>%s</name>' % name
-#     print >>openfile,'  <description><![CDATA['
-#     print >>openfile,'  <p>SeisComP3 Station Extracter st2kml<br/>'
-#     print >>openfile,']]>  </description>'
+    print (' <Folder>', file = openfile)
+    print ('  <name>%s</name>' % name, file = openfile)
+#     print ('  <description><![CDATA[', file = openfile)
+#     print ('  <p>SeisComP3 Station Extracter st2kml<br/>', file = openfile)
+#     print (']]>  </description>', file = openfile)
 
 '''
 KML Generators
 '''
 def openKML(openfile, options, styler):
-    print >>openfile,'<?xml version="1.0" encoding="UTF-8"?>'
-    print >>openfile,'<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2">'
-    print >>openfile,' <Document>'
+    print ('<?xml version="1.0" encoding="UTF-8"?>', file = openfile)
+    print ('<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2">', file = openfile)
+    print (' <Document>', file = openfile)
 
     if styler:
         styler.dump(openfile)
@@ -96,37 +96,37 @@ def ptKML(openfile, options, code, channels, start, end, lon, lat, ele, desc, rm
     if lon == None: return
     if lat == None: return
 
-    print >>openfile,'  <Placemark>'
+    print ('  <Placemark>', file = openfile)
     if style:
-        print >>openfile,'  <styleUrl>#%s</styleUrl>' % style
+        print ('  <styleUrl>#%s</styleUrl>' % style, file = openfile)
 
-    print >>openfile,'  <name>%s</name>' % (code.split(".")[1])
-    print >>openfile,'  <description><![CDATA['
+    print ('  <name>%s</name>' % (code.split(".")[1]), file = openfile)
+    print ('  <description><![CDATA[', file = openfile)
 
-    print >>openfile,"<pre>"
+    print ("<pre>", file = openfile)
     
-    print >>openfile,"<b>Description:</b> %s" % desc
+    print ("<b>Description:</b> %s" % desc, file = openfile)
     
-    print >>openfile,""
-    print >>openfile,"<b>Network:</b> %s" % (code.split(".")[0])
+    print ("", file = openfile)
+    print ("<b>Network:</b> %s" % (code.split(".")[0]), file = openfile)
     
-    print >>openfile,""
-    print >>openfile,"<b>Locations and Channels Names:</b>"
-    print >>openfile,"(SEED Standard Naming)"
+    print ("", file = openfile)
+    print ("<b>Locations and Channels Names:</b>", file = openfile)
+    print ("(SEED Standard Naming)", file = openfile)
 
     for chunk in re.findall('.{21}',channels) if len(channels) > 21 else [channels]:
-        print >>openfile,"  %s" % chunk
-    print >>openfile,""
+        print ("  %s" % chunk, file = openfile)
+    print ("", file = openfile)
 
-    print >>openfile,"<b>Operation Time:</b>"
-    print >>openfile,'  Start: %s' % start
-    print >>openfile,'    End: %s' % ("--" if end is None else end)
+    print ("<b>Operation Time:</b>", file = openfile)
+    print ('  Start: %s' % start, file = openfile)
+    print ('    End: %s' % ("--" if end is None else end), file = openfile)
 
-    print >>openfile,""
-    print >>openfile,"<b>Station Location:</b>"
-    print >>openfile,'  Longitude: %+09.4f' % lon
-    print >>openfile,'  Latitude:  %+09.4f' % lat
-    print >>openfile,'  Elevation: %6.1f (m)' % ele
+    print ("", file = openfile)
+    print ("<b>Station Location:</b>", file = openfile)
+    print ('  Longitude: %+09.4f' % lon, file = openfile)
+    print ('  Latitude:  %+09.4f' % lat, file = openfile)
+    print ('  Elevation: %6.1f (m)' % ele, file = openfile)
 
     method = {
      None: "Unset",
@@ -144,37 +144,37 @@ def ptKML(openfile, options, code, channels, start, end, lon, lat, ele, desc, rm
          "2G": "Online"
     }
 
-    print >>openfile,""
-    print >>openfile,"<b>Station Transmission:</b>"
+    print ("", file = openfile)
+    print ("<b>Station Transmission:</b>", file = openfile)
     if end is None:
-        print >>openfile,"  Status is %s" % online[rmk]
+        print ("  Status is %s" % online[rmk], file = openfile)
         if online[rmk] == "Online":
-            print >>openfile,"  Method: %s" % method[rmk]
+            print ("  Method: %s" % method[rmk], file = openfile)
     else:
-        print >>openfile,"  Status is closed."
+        print ("  Status is closed.", file = openfile)
 
-    print >>openfile,""
-    print >>openfile,"<b>Instruments in Station:</b>"
-    print >>openfile,"  %s ; %s" % ("--" if sensor is None else sensor, "--" if dtl is None else dtl)
-    print >>openfile,'</pre>]]></description>'
+    print ("", file = openfile)
+    print ("<b>Instruments in Station:</b>", file = openfile)
+    print ("  %s ; %s" % ("--" if sensor is None else sensor, "--" if dtl is None else dtl), file = openfile)
+    print ('</pre>]]></description>', file = openfile)
 
-    print >>openfile,'  <TimeSpan>'
-    print >>openfile,'    <begin>%s</begin>' % (start)
+    print ('  <TimeSpan>', file = openfile)
+    print ('    <begin>%s</begin>' % (start), file = openfile)
     if end:
-        print >>openfile,'    <end>%s</end>' % (end)
-    print >>openfile,'  </TimeSpan>'
+        print ('    <end>%s</end>' % (end), file = openfile)
+    print ('  </TimeSpan>', file = openfile)
 
-    print >>openfile,'   <Point>'
-    print >>openfile,'    <coordinates>%f,%f,%f</coordinates>' % (lon, lat, 0.0)
-    print >>openfile,'   </Point>'
-    print >>openfile,'  </Placemark>'
+    print ('   <Point>', file = openfile)
+    print ('    <coordinates>%f,%f,%f</coordinates>' % (lon, lat, 0.0), file = openfile)
+    print ('   </Point>', file = openfile)
+    print ('  </Placemark>', file = openfile)
 
 def closeFolder(openfile):
-    print >>openfile,' </Folder>'
+    print (' </Folder>', file = openfile)
 
 def closeKML(openfile):
-    print >>openfile,' </Document>'
-    print >>openfile,' </kml>'
+    print (' </Document>', file = openfile)
+    print (' </kml>', file = openfile)
 
 '''
 Scales
@@ -208,7 +208,7 @@ def datafromxml(filename):
     inv = DataModel.Inventory.Cast(obj)
 
     if type(inv) == type(None):
-        print >>sys.stderr,"File (%s) is no event, skipping." % filename
+        print ("File (%s) is no event, skipping." % filename, file = sys.stderr)
         return None
 
     return inv
@@ -222,7 +222,7 @@ def collect(sta):
             cha = loc.stream(cc)
             codes.append("%s.%s" % ("--" if loc.code() == "" else loc.code(), cha.code()))
 #             sensor=sta.network().inventory().findSensor(cha.sensor())
-#             print "MA='%s' MO='%s' DS='%s' %s" % (sensor.manufacturer(),sensor.model(),sensor.description(),sensor.name())
+#             print("MA='%s' MO='%s' DS='%s' %s" % (sensor.manufacturer(),sensor.model(),sensor.description(),sensor.name()))
 
     codes.sort(reverse = True)
     return ",".join(codes)
@@ -262,7 +262,7 @@ if __name__ == "__main__":
         options.filter = options.filter.split(",")
 
     for f in args:
-        print >>sys.stderr,"Processing file: %s" % f
+        print ("Processing file: %s" % f, file = sys.stderr)
         # Get data
         #
         inv = datafromxml(f)
@@ -279,9 +279,9 @@ if __name__ == "__main__":
                     d = datetime.datetime.strptime(end.toString("%Y-%m-%d %H:%M:%SZ"), "%Y-%m-%d %H:%M:%SZ")
                     open = "true" if d > datetime.datetime.now() else "false"
                     end = end.toString("%Y-%m-%dT%H:%M:%SZ")
-                except seiscomp3.Core.ValueException:
-                    end = None
-                    open = "true"
+                # ~ except seiscomp.Core.ValueException:
+                    # ~ end = None
+                    # ~ open = "true"
                 except ValueError:
                     end = None
                     open = "true"
@@ -333,8 +333,8 @@ if __name__ == "__main__":
                 where = records[open][net.code()]
 
                 if code in where:
-                    if data['end'] is None and where[code]['open'] is "false":
-                        print >>sys.stderr,"Over-write."
+                    if data['end'] is None and where[code]['open'] == "false":
+                        print ("Over-write.", file = sys.stderr)
                         where[code] = data
                 else:
                     where[code] = data
@@ -343,19 +343,19 @@ if __name__ == "__main__":
     #
     openKML(fio, options, styler)
 
-    for (k,g) in records.iteritems():
+    for (k,g) in records.items():
         if k == "false":
             newFolder(fio, "Stations Already closed")
         else:
             newFolder(fio, "Stations in Operation")
 
-        nkeys = g.keys()
+        nkeys = list(g.keys())
         nkeys.sort()
         for n in nkeys:
             net = g[n]
             newFolder(fio, "%s network (%d stations)" % (n,len(net)))
 
-            skeys = net.keys()
+            skeys = list(net.keys())
             skeys.sort()
             for s in skeys:
                 data = net[s]
